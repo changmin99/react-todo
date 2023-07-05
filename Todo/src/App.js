e499197c8a821caab7ff6e324f4dfb231d99781d
changmin99 <changmin8142@gmail.com>
2023-07-05 17:34:16 +0900

    final

--- Todo/src/App.js
+++ Todo/src/App.js
@@ -1,21 +1,22 @@
-import { useState } from 'react';
-import './App.css';
-import Header from './components/Header/Header';
-import TodoList from './components/TodoList/TodoList';
-// import { DrakModeProvider } from './context/DarkModeContext';
+import React, { useState } from "react";
+import "./App.css";
+import TodoList from "./components/TodoList/TodoList";
+import Header from "./components/Header/Header";
+import { DarkModeProvider } from "./context/DarkModeContext";
 
-const filters = ['all', 'active ' , 'completed'];
+const filters = ["all", "active", "completed"];
+
 function App() {
-  const [filter , setFilter] = useState(filters[0]);
-return (  
-  <div>
-    <Header 
-    filters={filters}
-    filter={filter}
-    onFilterChange={setFilter}
-    />
-    <TodoList filter={filter} />
-  </div>
+  const [filter, setFilter] = useState(filters[0]);
+  return (
+    <DarkModeProvider>
+      <Header
+        filters={filters}
+        filter={filter}
+        onFilterChange={/* (filter) => setFilter(filter) */ setFilter}
+      />
+      <TodoList filter={filter} />
+    </DarkModeProvider>
   );
 }
 
--- Todo/src/context/DarkModeContext.jsx
+++ Todo/src/context/DarkModeContext.jsx
@@ -1,38 +1,46 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { useContext } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { createContext } from "react";
 
 const DarkModeContext = createContext();
-
-
-export function DrakModeProvider({children}) {
-    const [darkmode, setDarkMode] = useState(false);
-    const toggleDarkMode = () =>{
-        setDarkMode(!darkmode);
-        updateDarkMode(!darkmode)
-        // 다크모드였던 반대로지정 (라이트모드)
-    }
-    // useEffect(()=>{
-    // const isDark =
-    // localStorage.theme === 'dark' || 
-    // (!('theme' in localStorage) && 
-    // window.matchMedia('(prefers-color-scheme: dark)').matches)
-    // setDarkMode(isDark);
-    // updateDarkMode(isDark);
-    // }, [])
-
-    return (
-    <DarkModeContext.Provider value={{darkmode,toggleDarkMode}}>
-        {children}
-    </DarkModeContext.Provider>
-    );
-}
-
-function updateDarkMode (darkmode) {
-    if(darkmode){
-        document.documentElement.classList.add('dark');
-        localStorage.theme='dark';
-    } else {
-        document.documentElement.classList.remove('dark');
-        localStorage.theme='light';
-    }
-}
 export const useDarkMode = () => useContext(DarkModeContext);
+
+export function DarkModeProvider({ children }) {
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    setDarkMode(/* (prev) => !prev */ !darkMode);
+    updateDarkMode(!darkMode);
+  };
+
+  useEffect(() => {
+    const isDark =
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches);
+    setDarkMode(isDark);
+    updateDarkMode(isDark);
+  }, []);
+
+  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark')
+  } else {
+    document.documentElement.classList.remove('dark')
+  }
+
+  return (
+    <DarkModeContext.Provider value={{ darkMode: darkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+function updateDarkMode(darkMode) {
+  if (darkMode) {
+    document.documentElement.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    document.documentElement.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+}
